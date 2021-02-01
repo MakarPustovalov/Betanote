b34576be109849fa99099c8752af36ae6a1fc3f0
MakarPustovalov <60263005+MakarPustovalov@users.noreply.github.com>
2021-02-01 14:45:34 +0300

    Created backbone for server

---
+++ Server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  jwt: {
+    accessSecret: "mysecret-dfgd4g43f3tfok3n3",
+    refreshSecret: "mysecret-lkm13diaxyz042w64",
+  },
+  port: 5000,
+  mongooseUrl: 'KhPlborEBSxHWYlj',
+  cors: {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+  }
+}
---
+++ Server/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const config = require('./config')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const indexRouter = require('./routes/indexRouter')
+const authRouter = require('./routes/authRouter')
+
+const app = express()
+
+const PORT = process.env.PORT || config.port
+
+app.use(cors(config.cors))
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json())
+app.use(cookieParser('secretkey-lc3bh532b'))
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method)
+  next()
+})
+
+app.use('/auth', authRouter)
+app.use('', indexRouter)
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.status || 400).json({message: err.message, error: err})
+})
+
+const start = () => {
+  try {
+    // mongoose.connect(config.mongooseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    app.listen(PORT, () => {
+      console.log('Server is running on port', PORT)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
