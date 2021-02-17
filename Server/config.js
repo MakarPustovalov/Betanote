38d8cf45c5057a50967235003ddf2619654c4cce
MakarPustovalov <60263005+MakarPustovalov@users.noreply.github.com>
2021-02-17 12:16:53 +0300

    Deployable version

--- Server/config.js
+++ Server/config.js
@@ -4,10 +4,10 @@ module.exports = {
     refreshSecret: "mysecret-lkm13diaxyz042w64",
   },
   port: 5000,
-  mongooseUrl: 'mongodb+srv://admin:KhPlborEBSxHWYlj@cluster0.iduaz.mongodb.net/betanote?retryWrites=true&w=majority',
-  domain: process.env.MODE === 'production' ? "makarpustovalov.github.io" : "localhost",
+  mongooseUrl: process.env.MDB || 'mongodb+srv://admin:KhPlborEBSxHWYlj@cluster0.iduaz.mongodb.net/betanote?retryWrites=true&w=majority',
+  domain: process.env.MODE === 'production' ? ".betanote-server.herokuapp.com" : "localhost",
   cors: {
-    origin: process.env.MODE === 'production' ? "https://makarpustovalov.github.io/Betanote" : "http://localhost:3000",
+    origin: process.env.MODE === 'production' ? "https://betanote-frontend.herokuapp.com" : "http://localhost:3000",
     optionsSuccessStatus: 200,
     allowedHeaders: ['Content-Type'],
     credentials: true
